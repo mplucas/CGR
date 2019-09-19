f6d921f625ea4f2c106043b0246c27eaaa3ef214
Lucas Meneghelli Pereira <lucas-m.p@hotmail.com>
2019-09-19 02:02:06 -0300

    Começado trabalho 3

---
+++ T3/shower.c
@@ -0,0 +1,178 @@
+// gcc shower.c -lglut -lGL -lGLU -lm
+
+#include <GL/glut.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+#define MAX_FLOCOS 500
+#define PI 3.1415
+
+// Rotation amounts
+static GLfloat xRot = 0.0f;
+static GLfloat yRot = 0.0f;
+float radius = 1.0f;
+float angle = 0.0f;
+
+typedef struct floco {
+	float pos[3];
+	float vel;
+} Floco;
+
+Floco gotas[MAX_FLOCOS];
+
+void cilinderRounded(GLUquadric* quad, GLdouble base, GLdouble top, GLdouble height){
+
+	gluCylinder(quad, base, top, height, 50, 25);
+	glTranslatef(0.0f, 0.0f, height);
+	gluSphere(quad, top, 50, 25);
+
+}
+
+// Change viewing volume and viewport.  Called when window is resized
+void ChangeSize(int w, int h) {
+    GLfloat fAspect;
+
+    // Prevent a divide by zero
+    if(h == 0)
+        h = 1;
+
+    // Set Viewport to window dimensions
+    glViewport(0, 0, w, h);
+
+    fAspect = (GLfloat)w/(GLfloat)h;
+
+    // Reset coordinate system
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    // Produce the perspective projection
+    gluPerspective(30.0f, fAspect, 1.0, 40.0);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+
+
+void init(){
+    glEnable(GL_DEPTH_TEST);    
+    glFrontFace(GL_CCW);        
+    glEnable(GL_CULL_FACE);     
+    glEnable(GL_COLOR_MATERIAL);
+    glColorMaterial(GL_FRONT, GL_AMBIENT_AND_DIFFUSE);
+    glClearColor(0.7f, 0.7f, 1.0f, 1.0f);
+
+	int i;
+	srand(time(NULL));
+	
+	for(i=0; i<MAX_FLOCOS; i++){
+		// Gerar um x aleatorio
+		float x = (float) (1.0 / ((rand() % 11) - 5));
+		x *= (rand() % 2 ? 1 : -1);
+	
+		float y = (float) (1.0 / ((rand() % 11) + 1));
+		y *= (rand() % 2 ? 1 : -1);
+
+		float z = (float) (1.0 / ((rand() % 11) - 5));
+		z *= (rand() % 2 ? 1 : -1);
+
+		gotas[i].pos[0] = x;
+		gotas[i].pos[1] = y;
+		gotas[i].pos[2] = z;
+
+		float v = (float) (1.0 / ((rand() % 50) + 100));
+		gotas[i].vel = v;
+	}
+}
+
+void OnDisplay(void){
+
+    GLUquadricObj *pObj = gluNewQuadric();
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    // +1
+    glPushMatrix();
+
+	// Move object back and do in place rotation
+	glTranslatef(0.0f, -0.5f, -5.0f);
+	glRotatef(xRot, 1.0f, 0.0f, 0.0f);
+	glRotatef(yRot, 0.0f, 1.0f, 0.0f);
+
+	gluQuadricNormals(pObj, GLU_SMOOTH);
+
+	// +2
+	glPushMatrix();
+
+	// Chuveiro
+	glColor3f(153.0/255.0f, 102.0/255.0f, 51.0/255.0f);
+
+	glRotatef(-90, 1.0f, 0.0f, 0.0f);
+	cilinderRounded( pObj, radius*0.02f, radius*0.02f, 1.0f );
+	
+	glRotatef(90, 1.0f, 0.0f, 0.0f);
+	cilinderRounded( pObj, radius*0.02f, radius*0.02f, 0.2f );
+
+	glRotatef(90, 1.0f, 0.0f, 0.0f);
+	cilinderRounded( pObj, radius*0.02f, radius*0.02f, 0.05f );
+	gluCylinder(pObj, radius*0.02f, radius*0.15f, 0.1f, 50, 25);
+	
+	glPopMatrix();
+	// -2
+	
+    glPopMatrix();
+
+	// gotas
+	glPushMatrix();
+
+	glTranslatef(0.0f, 0.0f, 0.1f);
+	glColor4f(1.0f, 1.0f, 1.0f, 0.2f);	
+	int i;
+
+	//PAREI AQUI
+
+	for(i=0; i<MAX_FLOCOS; i++){
+		glPushMatrix();
+		glTranslatef(gotas[i].pos[0], gotas[i].pos[1], gotas[i].pos[2]);
+		gluSphere(pObj, 0.02f, 12, 6);
+		gotas[i].pos[1] -= gotas[i].vel;
+		gotas[i].pos[0] += sin(angle)*0.001f;
+		gotas[i].pos[0] += cos(i)*0.001f;
+		if(gotas[i].pos[1] < -2.5)
+			gotas[i].pos[1] = 0.5;
+		if(gotas[i].pos[0] < -2 || gotas[i].pos[0] > 2){
+			gotas[i].pos[0] *= -1;
+			if(gotas[i].pos[0] >= 2)
+				gotas[i].pos[0] -= 0.3;
+			else if(gotas[i].pos[0] <= -2)
+				gotas[i].pos[0] += 0.3;
+		}
+		glPopMatrix();
+	}
+	angle+=0.1f;
+	glPopMatrix();
+	
+    // Buffer swap
+    glutSwapBuffers();
+
+    yRot = (GLfloat)((const int)yRot % 360);
+    yRot++;
+
+    glutPostRedisplay();
+}
+
+
+int main(int argc, char *argv[]){
+
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
+    glutInitWindowSize(800, 600);
+    glutCreateWindow("Modeling with Quadrics");
+    glutReshapeFunc(ChangeSize);
+    glutDisplayFunc(OnDisplay);
+    init();
+    glutMainLoop();
+
+    return 0;
+
+}
